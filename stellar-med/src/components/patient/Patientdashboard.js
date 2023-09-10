a6343b573b81fb495dd5caa32b647da5b3806fe5
Kinjal2004 <bkinjal2004@gmail.com>
2023-09-10 14:04:58 +0530

    updated patient pages

--- stellar-med/src/components/patient/Patientdashboard.js
+++ stellar-med/src/components/patient/Patientdashboard.js
@@ -1,55 +1,77 @@
-import { Fragment, useState } from 'react'
-import Navbar from './Navbar'
-import Patientchat from './Patient-Chat/Patientchat';
-import { useLocation } from 'react-router-dom';
+import { Fragment, useState } from "react";
+import Navbar from "./Navbar";
+import Patientchat from "./Patient-Chat/Patientchat";
+import { useLocation } from "react-router-dom";
+import Patientreminder from "./Patientreminder";
+import { Link } from "react-router-dom";
+import Report from "../reports/Report";
+
 
 export default function Patientdashboard() {
-  const [showChat,setShowChat] = useState(false);
+  const [showChat, setShowChat] = useState(false);
 
-  const handleClick =()=>{
+  const handleClick = () => {
     setShowChat(!showChat);
-  }
+  };
   const location = useLocation();
   const id = new URLSearchParams(location.search).get("id");
 
   let myStyles = {
-    backgroundImage: `url("https://img.freepik.com/free-photo/blur-hospital_1203-7969.jpg?w=1060&t=st=1693410891~exp=1693411491~hmac=44ad241149d6f75c6bd61a3448eaae807f39a7a747b38373c6df95f641e5eadf")` ,
-    height:'91vh',
-    backgroundSize: 'cover',
-   }
-   
+    backgroundImage: `url("https://img.freepik.com/free-photo/blur-hospital_1203-7969.jpg?w=1060&t=st=1693410891~exp=1693411491~hmac=44ad241149d6f75c6bd61a3448eaae807f39a7a747b38373c6df95f641e5eadf")`,
+    height: "91vh",
+    backgroundSize: "cover",
+  };
+
   return (
-    
     <>
-      <Navbar/>
-      <div className="min-h-full" style={myStyles}>
+      <Navbar />
+      <div className="min-h-full overflow-hidden" style={myStyles} >
         <header className="">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+              Dashboard
+            </h1>
           </div>
         </header>
         <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <div className="container h-screen w-3/4 bg-slate-100 opacity-90 mx-auto flex flex-row rounded-xl">
+               <div className="container  h-screen w-64 flex flex-col  ">
+                      <div className="container bg-slate-200 h-64 m-3 rounded-xl flex justify-center items-center">
+                      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded h-48">
+                            <img src="https://lordicon.com/icons/wired/gradient/1249-heart-beat.svg" alt="" className="h-36 w-36"/>
+                            <Link id="heartbeat" to="https://app.hyperate.io/74B6">Monitor Heart Rate</Link>
+                      </button>
+ 
+                      </div>
+                      <div className="container bg-slate-400 h-1/2 m-3 rounded-xl">
+                            
+                            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                                 <Patientreminder data={id} />
+                            </div>
+                      </div>
+               </div>
+               <div className="container bg-slate-300 h-3/4 w-3/4 mx-8 my-5 rounded-xl">
+               <Report data={id}/>
+               </div>
+          </div>
+
+
         </main>
       </div>
       <div className="fixed bottom-10 left-10">
-      <button className="w-12 h-12 rounded-full bg-blue-500 text-white flex items-center justify-center"
-      onClick={handleClick}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-        </svg>
-        <img
-      className="h-15 w-15 rounded-full object-cover object-center"
-      src="https://static-00.iconduck.com/assets.00/chat-icon-512x512-nre05atn.png"
-      alt="Chat with us"
-    />
-      </button>
-      {showChat && <Patientchat data={id}/>}
+        <button
+          className="w-12 h-12 rounded-full bg-blue-500 text-white flex items-center justify-center sticky"
+          style={{ bottom: showChat ? "10px" : "initial" }} // Add this style
+          onClick={handleClick}
+        >
+          <img
+            className="h-15 w-15 rounded-full object-cover object-center sticky"
+            src="https://static-00.iconduck.com/assets.00/chat-icon-512x512-nre05atn.png"
+            alt="Chat with us"
+          />
+        </button>
+        {showChat && <Patientchat data={id} />}
       </div>
-
-
     </>
-
-  )
+  );
 }
---
+++ stellar-med/src/components/patient/Patientreminder.js
@@ -0,0 +1,60 @@
+import { doc, collection, getDocs, query, orderBy } from "firebase/firestore";
+import { db } from "../../firebase";
+import React, { useEffect, useState } from "react";
+
+export default function PatientReminder(props) {
+  const [medicines, setMedicines] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch medicines
+    const fetchMedicines = async () => {
+      try {
+        console.log("Fetching medicines...");
+        const patientId = props.data;
+        const patientRef = doc(db, "Patient", patientId);
+        const medicinesRef = collection(patientRef, "medicine-reminder");
+
+        // Create a query for medicines, ordered by createdAt
+        const q = query(medicinesRef);
+
+        const snapshot = await getDocs(q);
+        const newMedicines = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        // Update medicines state once at the end
+        setMedicines(newMedicines);
+        console.log("Medicines retrieved:", newMedicines);
+      } catch (error) {
+        console.error("Error fetching medicines:", error);
+      }
+    };
+
+    // Call fetchmedicines when the component mounts
+    console.log("Calling fetchMedicines...");
+    fetchMedicines();
+  }, [props.data]);
+
+  console.log("Medicines state:", medicines);
+
+  return (
+    <>
+      <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg">
+        <li className="w-full px-4 py-2 border-b border-gray-200  font-bold text-lg text-blue-600">
+          Medicines
+        </li>
+        {medicines.map((meds) => (
+          <li
+            className="w-full px-4 py-2 border-b border-gray-200"
+            key={meds.id}
+          >
+            {meds.name}
+          </li>
+        ))}
+      </ul>
+
+      {/* Render other elements here */}
+    </>
+  );
+}
